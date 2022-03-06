2799dcfd16b0fc5b4eeb8d3980d12781e679ecda
samankey <71953671+samankey@users.noreply.github.com>
2022-03-07 00:03:49 +0900

    Add: modify userDao

--- models/UserDao.js
+++ models/UserDao.js
@@ -1,29 +1,21 @@
 import prisma from '../prisma';
 
-const createUser = async (nickname, email, hashedPw) => {
-  return await prisma.user.create({
-    data: {
-      nickname,
-      email,
-      password: `${hashedPw}`
-    }
-  });
+const createUser = async (email, hashedPw, nickname) => {
+  return await prisma.$queryRaw(`
+    INSERT INTO users (email, password, nickname) VALUES ('${email}, ${hashedPw}, ${nickname}')
+  `);
 };
 
 const findUserByEmail = async (email) => {
-  return await prisma.user.findUnique({
-    where: {
-      email
-    }
-  });
+  return await prisma.$queryRaw(`
+    SELECT email FROM users WHERE email='${email}'
+  `);
 };
 
 const findUserByNickname = async (nickname) => {
-  return await prisma.user.findUnique({
-    where: {
-      nickname
-    }
-  })
+  return await prisma.$queryRaw(`
+    SELECT email FROM users WHERE nickname='${nickname}'
+  `);
 }
 
 export default { createUser, findUserByEmail, findUserByNickname };
